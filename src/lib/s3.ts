590363aaf7ff202e06757bb642f0c8cc1b0fb827
Deiby Moreno <d.moreno@visiontravel.net>
2026-08-18 15:08:48 -0500

    chore: updated env

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -54,10 +54,10 @@ export async function createPresignedUploadUrl(options: {
   }
 
   const client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.NEXT_AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+      accessKeyId: process.env.NEXT_AWS_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY ?? "",
     },
   });
 
